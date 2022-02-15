76834710b1c7e06887c64848a8608ca6f62a2183
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2022-02-15 23:37:30 +0900

    add

---
+++ content.js
@@ -0,0 +1,6 @@
+console.log('Sample App Start!');
+window.onload = function() {
+  const body = document.getElementsByClassName('gb_na gb_Za gb_Oe gb_Kc NkK3Fc');
+  console.log(body);
+  body[0].style.backgroundColor = '#000000';
+};
