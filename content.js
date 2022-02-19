2b4707240c29c3b19b8ae99f72796711affc8677
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2022-02-19 20:01:23 +0900

    add

--- content.js
+++ content.js
@@ -1,6 +1,17 @@
-console.log('Sample App Start!');
-window.onload = function() {
-  const body = document.getElementsByClassName('gb_na gb_Za gb_Oe gb_Kc NkK3Fc');
-  console.log(body);
-  body[0].style.backgroundColor = '#000000';
-};
+window.addEventListener('load', function () {
+  console.log('load!');
+  // FIXME
+  setTimeout(function () {
+    const button = document.createElement("gmail_button");
+    button.innerHTML = "Gmail";
+    // https://developer.mozilla.org/ja/docs/Web/CSS/cursor#%E5%85%AC%E5%BC%8F%E5%AE%9A%E7%BE%A9
+    button.style.cursor = "pointer";
+    button.onclick = function () {
+      const url = 'https://mail.google.com/mail/u/0/#inbox';
+      window.open(url, '_blank');
+    };
+    const targetDiv = document.getElementsByClassName('sc-6qqf7t-6');
+    targetDiv[0].insertBefore(button, null);
+  }, 1000);
+
+});
